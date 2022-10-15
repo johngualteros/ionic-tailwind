39f4d8931e975a8fe9f16088b218f029c9426a0d
John Gualteros <gualterosjohn40@gmail.com>
2022-10-15 17:36:54 -0500

    Final commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ionic + tailwind css example
+
+![](./src/assets/ionic_tw_1.png)
+![](./src/assets/ionic_tw_2.png)
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChild(IonModal) modal: IonModal;
 
+  message = 'Learn Ionic';
+  messageTailwind = 'Learn Tailwind';
+  name: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+  cancelTailwind() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+  confirmTailwind() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Thanks for visit the ionic details`;
+    }
+  }
+  onWillDismissTailwind(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.messageTailwind = `Thanks for visit the tailwind details`;
+    }
+  }
 }
